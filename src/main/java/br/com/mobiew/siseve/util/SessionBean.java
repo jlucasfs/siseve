2ec11e6bb9dbb60abe59977ab3d45ff67881379c
jlucasfs <jlucasfs@gmail.com>
2017-12-07 22:56:42 -0200

    correcoes usuario logado, titulo do sistema com o evento atual, telas de atendimento e servico - foco no nome

--- src/main/java/br/com/mobiew/siseve/util/SessionBean.java
+++ src/main/java/br/com/mobiew/siseve/util/SessionBean.java
@@ -3,7 +3,10 @@ package br.com.mobiew.siseve.util;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.PieChartModel;
@@ -15,6 +18,8 @@ import br.com.mobiew.siseve.dto.PacienteDTO;
 import br.com.mobiew.siseve.model.entity.Evento;
 import br.com.mobiew.siseve.model.entity.Usuario;
 import br.com.mobiew.siseve.service.ClienteService;
+import br.com.mobiew.siseve.service.EventoService;
+import br.com.mobiew.siseve.service.UsuarioService;
 import br.com.mobiew.siseve.util.scopes.Scopes;
 
 /**
@@ -37,6 +42,12 @@ public class SessionBean {
 	@Autowired
 	private ClienteService clienteService;
 
+	@Autowired
+	private EventoService eventoService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	private PacienteDTO pacienteDto;
 
 	private List<PacienteDTO> aniversariantesDto;
@@ -47,12 +58,26 @@ public class SessionBean {
 
 	private PieChartModel listaAtendimentosPorServico;
 
+	private String tituloSistema;
+	
 	@PostConstruct
 	public void inicializar() {
 		try {
+	    	HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession( true );
+	    	if ( session.getAttribute( Constantes.USUARIO_AUTENTICADO ) == null ) {
+		    	Usuario usuario = this.usuarioService.findByLoginSenha( "admin", "admin" );
+		    	session.setAttribute( Constantes.USUARIO_AUTENTICADO, usuario );
+	    	}
+
 			this.pacienteDto = this.clienteService.findPacientes();
 			this.listaAtendimentosSeisMeses = new CartesianChartModel();
 			this.listaAtendimentosPorServico = new PieChartModel();
+			Evento eventoAtual = this.eventoService.findEventoAtual();
+			if ( eventoAtual == null ) {
+				this.tituloSistema = StringUtils.EMPTY;
+			} else {
+				this.tituloSistema = eventoAtual.getNome();
+			}
 		} catch ( Exception e ) {
 			LOG.error( "Erro nao esperado ao inicializar o bean de escope de sessao que prepara os dados a serem exibidos na tela Home - " + e.getMessage() );
 			e.printStackTrace();
@@ -129,6 +154,16 @@ public class SessionBean {
 
 	public void setListaAtendimentosSeisMeses( CartesianChartModel listaAtendimentosSeisMesesParam ) {
 		this.listaAtendimentosSeisMeses = listaAtendimentosSeisMesesParam;
+	}
+
+	
+	public String getTituloSistema() {
+		return this.tituloSistema;
+	}
+
+	
+	public void setTituloSistema( String tituloSistemaParam ) {
+		this.tituloSistema = tituloSistemaParam;
 	}
 
 }
